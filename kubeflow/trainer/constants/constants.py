6f053d0497a42cbf1a6ce5f8d5151ad363f78eff
Saad Zaher <szaher@redhat.com>
2025-09-15 13:11:24 +0100

    let linter handle line-length \n
    add labels next to linters codes
    
    Signed-off-by: Saad Zaher <szaher@redhat.com>

--- kubeflow/trainer/constants/constants.py
+++ kubeflow/trainer/constants/constants.py
@@ -116,7 +116,10 @@ POD_PENDING = "Pending"
 
 # The label selector for Pods created by the TrainJob.
 # It checks the following rJob.name: dataset-initializer, model-initializer, launcher, node.
-POD_LABEL_SELECTOR = f"{JOBSET_NAME_LABEL}={{trainjob_name}},{JOBSET_RJOB_NAME_LABEL} in ({DATASET_INITIALIZER}, {MODEL_INITIALIZER}, {LAUNCHER}, {NODE})"
+POD_LABEL_SELECTOR = (
+    f"{JOBSET_NAME_LABEL}={{trainjob_name}},{JOBSET_RJOB_NAME_LABEL} "
+    f"in ({DATASET_INITIALIZER}, {MODEL_INITIALIZER}, {LAUNCHER}, {NODE})"
+)
 
 # Handle environment variable for multiple URLs (comma-separated).
 # The first URL will be the index-url, and remaining ones are extra-index-urls.
